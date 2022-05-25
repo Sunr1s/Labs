6ca4fd5e406141de46073adbf1977cee913262ad
Sunr1s <david_gosha@knu.ua>
2022-05-25 15:27:58 +0300

    om_zalik

---
+++ БД/BD11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace BD11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
