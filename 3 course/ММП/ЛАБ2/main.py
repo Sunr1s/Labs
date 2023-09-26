7412efa801554c65ee3aced6ee679aa73b2c451d
Sunr1se <david_gosha@knu.ua>
2023-09-26 15:54:28 +0300

    AI predictor

--- 3 course/ММП/ЛАБ2/main.py
+++ 3 course/ММП/ЛАБ2/main.py
@@ -1,65 +1,58 @@
-import numpy as np
-import pandas as pd
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import seaborn as sns
-import warnings; warnings.filterwarnings(action='once')
 import os
-import random
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+import matplotlib.patches as patches
 
+sns.set_context("poster")  # It makes the labels, titles, etc. larger for better readability.
 
-df = pd.read_excel("C:/Users/admin/Desktop/Labs/3 course/ММП/ЛАБ2/11.xlsx")
+BASE_DIR = "C:/Users/admin/Desktop/Labs/3 course/ММП/ЛАБ2"
 
-def all_distinct():
-    # Draw Plot
+def load_data(file_name):
+    return pd.read_excel(os.path.join(BASE_DIR, file_name))
+
+def draw_density_plot(title, data, labels, colors):
     plt.figure(figsize=(13,10), dpi= 80)
-    plt.title('Density Plot of Time and all Transaction', fontsize=22)
-    sns.distplot(df['time_per_block'], color="dodgerblue", label="All trsx", hist_kws={'alpha':.7}, kde_kws={'linewidth':3})
-    plt.ylabel='Validity Сonfirmation'
-    plt.xlabel='Time'
-    plt.ylim(0, 0.2)
-    # Decoration
-    plt.legend()
+    plt.title(title, fontsize=22)
+    for label, color in zip(labels, colors):
+        sns.kdeplot(data[label], color=color, label=label, linewidth=3)
+    plt.legend(fontsize=20)
     plt.show()
 
-def per_trnsx():
-    # Draw Plot
-    plt.figure(figsize=(13,10), dpi= 80)
-    sns.distplot(df.loc[df['transaction_count'] < 500, "time_per_block"], color="dodgerblue", label="- 500", hist_kws={'alpha':.7}, kde_kws={'linewidth':3})
-    sns.distplot(df.loc[(df['transaction_count'] < 1000) & (df['transaction_count'] > 500), "time_per_block"], color="orange", label="500 - 1000", hist_kws={'alpha':.7}, kde_kws={'linewidth':3})
-    sns.distplot(df.loc[(df['transaction_count'] < 2000) & (df['transaction_count'] > 1000), "time_per_block"], color="g", label="1000 - 2000", hist_kws={'alpha':.7}, kde_kws={'linewidth':3})
-    sns.distplot(df.loc[df['transaction_count'] > 2000, "time_per_block"], color="deeppink", label="2000 +", hist_kws={'alpha':.7}, kde_kws={'linewidth':3})
-    plt.ylim(0, 0.2)
+def all_distinct(df):
+    draw_density_plot(
+        'Density Plot of Time and all Transaction', 
+        df,
+        ['time_per_block'], 
+        ["dodgerblue"]
+    )
 
-    # Decoration
-    plt.title('Density Plot of City Transaction by Count', fontsize=22)
-    plt.legend()
+def per_trnsx(df):
+    labels = ['- 500', '500 - 1000', '1000 - 2000', '2000 +']
+    colors = ['dodgerblue', 'orange', 'green', 'deeppink']
+    data = df.loc[df['transaction_count'] < 500, "time_per_block"]
+    for i in range(500, 2000, 500):
+        data = pd.concat([data, df.loc[(df['transaction_count'] < i+500) & (df['transaction_count'] > i), "time_per_block"]], axis=1)
+    data = pd.concat([data, df.loc[df['transaction_count'] > 2000, "time_per_block"]], axis=1)
+    data.columns = labels
+    draw_density_plot(
+        'Density Plot of City Transaction by Count', 
+        data,
+        labels,
+        colors
+    )
+
+def bar_trnsx(df):
+    fig, ax = plt.subplots(figsize=(13,10), dpi= 80)
+    ax.bar(df['transaction_count'], df['time_per_block'], width=40)
     plt.show()
 
-def bar_trnsx():
-    x = df['transaction_count'] 
-    y = df['time_per_block'] 
-
-    fig, ax = plt.subplots()
-
-    ax.bar(x, y,width = 40)
-
-    ax.set_facecolor('seashell')
-    fig.set_facecolor('floralwhite')
-    fig.set_figwidth(12)    #  ширина Figure
-    fig.set_figheight(6)    #  высота Figure
-
-    plt.show()
-
-def new(x):
-    df_raw = pd.read_excel("C:/Users/admin/Desktop/Labs/3 course/ММП/ЛАБ2/12.xlsx")
+def new(df, sort):
+    df_raw = load_data("12.xlsx")
     df = df_raw[['transaction_count', 'median_time','time_per_block']].groupby('median_time').apply(lambda x: x.mean())
-    if x:
+    if sort:
         df.sort_values('transaction_count', inplace=True)
     df.reset_index(inplace=True)
-
-    # Draw plot
-    import matplotlib.patches as patches
 
     fig, ax = plt.subplots(figsize=(16,10), facecolor='white', dpi= 80)
     ax.vlines(x=df.index, ymin=0, ymax=df.transaction_count, color='firebrick', alpha=0.7, linewidth=20)
@@ -68,22 +61,15 @@ def new(x):
     for i, trx in enumerate(df.transaction_count):
         ax.text(i, trx+0.5, round(trx, 1), horizontalalignment='center')
 
-
-    # Title, Label, Ticks and Ylim
-    ax.set_title('Bar Chart for Transaction Slow Blocks', fontdict={'size':22})
+    ax.set_title('Bar Chart for Transaction Slow Blocks', fontsize=22)
     ax.set(ylabel='Transaction per block', ylim=(0, 4000))
-     #plt.xticks(df.index, df.median_time, rotation=60, horizontalalignment='right', fontsize=12)
 
-    # Add patches to color the X axis labels
     p1 = patches.Rectangle((.57, -0.005), width=.33, height=.13, alpha=.1, facecolor='green', transform=fig.transFigure)
     p2 = patches.Rectangle((.124, -0.005), width=.446, height=.13, alpha=.1, facecolor='red', transform=fig.transFigure)
     fig.add_artist(p1)
     fig.add_artist(p2)
-    
-    
 
-    # y = df['transaction_count'] 
-    if x:
+    if sort:
         ax.plot(df.sort_values(by=['transaction_count'])['time_per_block'] * 50)
     else:
         ax.plot(df['time_per_block'] * 50)
@@ -92,18 +78,17 @@ def new(x):
 
 if __name__ == '__main__':
     os.system('clear')
-    print("Enter func ")
+    print("Enter the number corresponding to the function you want to use:\n1. all_distinct\n2. per_trnsx\n3. bar_trnsx\n4. new(False)\n5. new(True)")
     term = input(">")
-    match term:
-        case '1':
-            all_distinct()
-        case '2':
-            per_trnsx()
-        case '3':
-            bar_trnsx()
-        case '4':
-            new(False)
-        case '5':
-            new(True)
-        case _:
-            print("try again")
+    df = load_data("11.xlsx")
+    functions = {
+        '1': all_distinct,
+        '2': per_trnsx,
+        '3': bar_trnsx,
+        '4': lambda df: new(df, False),
+        '5': lambda df: new(df, True),
+    }
+    try:
+        functions[term](df)
+    except KeyError:
+        print("Invalid input. Please try again.")
